b2e55c3ba9f8c5fac3ea6a8605a055fb2fb150fb
pardha <pardha2002@gmail.com>
2025-04-03 10:31:22 +0530

    apr3

---
+++ str_func.py
@@ -0,0 +1,9 @@
+str='vishnu vardhan'
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.count("i"))
+print(str.find('v'))#first occurance of index
+print(str.split(' '))
+print(str.replace('vardhan','allamsetti'))
+# print(str.append('A'))
