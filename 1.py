c99c0f3463ca9a636a6b11dd27f918ad2b5dcb97
pardha <pardha2002@gmail.com>
2025-04-03 09:59:54 +0530

    3apr

---
+++ 1.py
@@ -0,0 +1,16 @@
+str='vishnu is my name'
+print(type(str))
+print(str[1])
+print(str[2])
+print(str[-1])
+# create a list and accessing
+l=[1,2,3,4,5]
+mixedd=['vishnu',1,'rahool',2,'basha',3,'rohit',4]
+print(l,mixedd)
+l=l+[4]
+print(mixedd[1:3])
+# print(l[-6])#array index out of bound
+tup1=(1,2,3,4,5)
+print(tup1[1])
+print(tup1[2])
+print(tup1[-1])
